7e76a4af097cd1052e8f7c76aed2fa191790167b
Harshal Ranjhani <ranjhaniharshal@gmail.com>
2024-06-12 12:41:55 +0530

    ui:no queries found message in the queries box

--- components/recent-questions.tsx
+++ components/recent-questions.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useSelector } from 'react-redux';
 
@@ -8,18 +8,28 @@ export function RecentQuestions() {
     <div className="h-[280px] overflow-auto">
       {questions.map((question: any, index: number) => {
         return (
-          <div className="flex flex-row shawdow-lg drop-shadow-lg hover:shadow-lg p-2 rounded-xl my-2" key={index}>
-          <Avatar className="h-9 w-9">
-            <AvatarImage src="/avatars/01.png" alt="Avatar" />
-            <AvatarFallback>{question.query.slice(0, 1)}</AvatarFallback>
-          </Avatar>
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">{question.query}</p>
-            <p className="text-sm text-muted-foreground">{question.status}</p>
+          <div
+            className="shawdow-lg my-2 flex flex-row rounded-xl p-2 drop-shadow-lg hover:shadow-lg"
+            key={index}
+          >
+            <Avatar className="h-9 w-9">
+              <AvatarImage src="/avatars/01.png" alt="Avatar" />
+              <AvatarFallback>{question.query.slice(0, 1)}</AvatarFallback>
+            </Avatar>
+            <div className="ml-4 space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {question.query}
+              </p>
+              <p className="text-sm text-muted-foreground">{question.status}</p>
+            </div>
           </div>
-        </div>
         );
       })}
+      {questions.length === 0 && (
+        <div className="flex h-full items-center justify-center">
+          <p className="text-muted-foreground">No queries found</p>
+        </div>
+      )}
     </div>
   );
 }
