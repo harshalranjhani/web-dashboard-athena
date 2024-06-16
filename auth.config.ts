83c91d7d87b0b06a94a53258ca9050130c7820ea
Harshal Ranjhani <ranjhaniharshal@gmail.com>
2024-06-16 12:12:16 +0530

    feat: github auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,60 @@
 import { NextAuthConfig } from 'next-auth';
 import CredentialProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
+import mongoose from 'mongoose';
+
+// MongoDB User schema
+const userSchema = new mongoose.Schema({
+  authType: String,
+  name: String,
+  email: String,
+  password: String,
+  image: String
+});
+
+const User = mongoose?.models?.User || mongoose.model('User', userSchema);
+
+// Connect to MongoDB
+const connectToDatabase = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+  await mongoose.connect(process.env.MONGODB_URI as string, {});
+};
 
 const authConfig = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+      async profile(profile) {
+        await connectToDatabase();
+
+        const users = await User.find({});
+
+        let user = await User.findOne({ email: profile.email });
+        if (!user) {
+          if (users.length >= 20) {
+            throw new Error(
+              'You cannot signup at this time. Please try again later.'
+            );
+          }
+          user = new User({
+            image: profile.avatar_url,
+            name: profile.name,
+            email: profile.email,
+            password: '',
+            authType: 'github'
+          });
+          await user.save();
+        }
+
+        return {
+          name: user.name,
+          email: user.email,
+          image: user.image
+        };
+      }
     }),
     CredentialProvider({
       credentials: {
@@ -36,7 +84,7 @@ const authConfig = {
     })
   ],
   pages: {
-    signIn: '/' //sigin page
+    signIn: '/' // Sign-in page
   }
 } satisfies NextAuthConfig;
 
